6587292dce1a2aa0835f18aff69967d92b0fb167
dwh1023 <dawei.hu@aliyun.com>
2017-04-18 09:57:30 +0800

    Update build.sh

--- ContainerCloud/images/logspout/build.sh
+++ ContainerCloud/images/logspout/build.sh
@@ -8,7 +8,7 @@ export LOGSPOUT_URL=https://github.com/gliderlabs/logspout/archive
 apk upgrade
 apk update
 apk add curl jq
-apk add --virtual .build-deps wget go git build-base
+apk add --virtual .build-deps wget go git svn build-base
 
 mkdir -p /go/src/github.com/gliderlabs
 
@@ -19,12 +19,14 @@ wget -O /tmp/v${LOGSPOUT_VERSION}.tar.gz \
   ln -s /go/src/github.com/gliderlabs/logspout-${LOGSPOUT_VERSION} \
     /go/src/github.com/gliderlabs/logspout
 
+svn co https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka
+
 cd /go/src/github.com/gliderlabs/logspout
 go get -x # || find ${GOPATH} && go env && find ${GOROOT}
 #sed 's/\/context/github\.com\/gorilla\/context/' /go/src/github.com/fsouza/go-dockerclient/client_unix.go > /go/src/github.com/fsouza/go-dockerclient/client_unix.go.tmp
 #mv /go/src/github.com/fsouza/go-dockerclient/client_unix.go.tmp /go/src/github.com/fsouza/go-dockerclient/client_unix.go
 cat /go/src/github.com/fsouza/go-dockerclient/client_unix.go
-go build -v -x -ldflags "-X main.Version dev" -o /bin/logspout
+#go build -v -x -ldflags "-X main.Version dev" -o /bin/logspout
 
 #apk del .build-deps
 #rm -rf /go
